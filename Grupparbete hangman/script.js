a09cc9d342145482058816d8e6aebbe463125850
HannisGo1 <Hannakarlsson69@gmail.com>
2023-11-22 10:16:27 +0100

    html css script

--- Grupparbete hangman/script.js
+++ Grupparbete hangman/script.js
@@ -2,6 +2,11 @@ const jsonData = required('./svenska-ord.json')
 
 //knapparnas id: lätt, medium, svår ! 
 // Math.Random () för att slumpa fram ord från listan! 
+// input fältet för :: inputName   
+
 let lättButton = document.querySelector('buttonLätt')
 let mediumButton = document.querySelector('buttonMedium')
 let svårButton = document.querySelector('buttonSvår')
+let nameInput = document.querySelector('inputName')
+let buttonStart = document.querySelector('startButton')
+let buttonHighscore = document.querySelector('highscoreButton')
