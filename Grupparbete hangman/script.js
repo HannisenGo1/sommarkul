dee31b50710af75ca70bbbf51438102329eff541
HannisGo1 <Hannakarlsson69@gmail.com>
2023-11-22 15:41:37 +0100

    tillagd grupparbete

--- Grupparbete hangman/script.js
+++ Grupparbete hangman/script.js
@@ -60,6 +60,8 @@ function handleIncorrectGuess() {
 
 }
 
+// Alfabetet i en lista: 
+const alfabetet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Å","Ä", "Ö"];
 
 
 
