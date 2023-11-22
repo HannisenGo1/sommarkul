a80e934ec64015d047cbc47c52317885e120bdcb
HannisGo1 <Hannakarlsson69@gmail.com>
2023-11-22 09:58:31 +0100

    tillagda rader script css index

--- Grupparbete hangman/script.js
+++ Grupparbete hangman/script.js
@@ -2,4 +2,6 @@ const jsonData = required('./svenska-ord.json')
 
 //knapparnas id: lätt, medium, svår ! 
 // Math.Random () för att slumpa fram ord från listan! 
-// 
+let lättButton = document.querySelector('buttonLätt')
+let mediumButton = document.querySelector('buttonMedium')
+let svårButton = document.querySelector('buttonSvår')
