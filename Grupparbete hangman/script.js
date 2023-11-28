ee30f66d66704032d3762b9fcb503fa09659e342
Anna <acarlssong@hotmail.com>
2023-11-28 16:08:54 +0100

    Grönt+rött; gamestate-view, inaktiverade knappar

--- Grupparbete hangman/script.js
+++ Grupparbete hangman/script.js
@@ -23,7 +23,7 @@ const viewPlay = document.querySelector('.playView');
 const labelName = document.querySelector('.divForName');
 const buttonBack = document.querySelector('#backButton');
 const tangentbord = document.querySelector('.keyboard-container');
-const tangenter = document.querySelector('.key')
+const tangent = document.querySelectorAll('.key')
 const svårighetsgradText = document.querySelector('.svårighetsgradText');
 const startMeny = document.querySelector('.startOchHigh')  //div m start o highscore
 const svårighetsgradDiv = document.querySelector('.svårighetsgrad');
@@ -42,6 +42,7 @@ let correctGuess = 0
 let totalGuess = 0
 let userName = ''
 let sparadNamn = ''
+let gameState = 'start-view'
 const startForButtonDiv = document.querySelector('.buttonForStart');
 const highScoreDiv = document.querySelector('.highscorediv');
 const easywords = words.filter((word) => word.length > 14)
@@ -56,32 +57,33 @@ gameoverButton.classList.add('hidden'); //tillbaka,kör igen knapparna
 startMeny.classList.add('visible')
 
 buttonStart.addEventListener('click', () => {
-if (buttonStart) {
-startForButtonDiv.classList.add('hidden');
-svårighetsgradDiv.classList.add('visible');
-svårighetsgradText.classList.add('visible');
-highScoreDiv.classList.add('hidden'); 
-labelName.classList.add('hidden');
-sparaNamn()
-startMeny.classList.add('visible')
-}
+	if (buttonStart) {
+		startForButtonDiv.classList.add('hidden');
+		svårighetsgradDiv.classList.add('visible');
+		svårighetsgradText.classList.add('visible');
+		highScoreDiv.classList.add('hidden'); 
+		labelName.classList.add('hidden');
+		sparaNamn()
+		startMeny.classList.add('visible')
+	}
 });
 
 
 buttonHighscore.addEventListener('click', () => {
-if (buttonHighscore){
-startForButtonDiv.classList.add('hidden');
-svårighetsgradDiv.classList.add('hidden');
-svårighetsgradText.classList.add('hidden');
-highScoreDiv.classList.add('hidden'); 
-startMeny.classList.add('hidden');
-labelName.classList.add('hidden');
-highScoreWindow.classList.remove('hidden');
+	if (buttonHighscore){
+	startForButtonDiv.classList.add('hidden');
+	svårighetsgradDiv.classList.add('hidden');
+	svårighetsgradText.classList.add('hidden');
+	highScoreDiv.classList.add('hidden'); 
+	startMeny.classList.add('hidden');
+	labelName.classList.add('hidden');
+	highScoreWindow.classList.remove('hidden');
 
-// göra en highscore div som sparar & visar namnen! 
-// det gamla spelet måste börja på 0 när man trycker på "kör igen"
-}
-});
+	// göra en highscore div som sparar & visar namnen! 
+	// det gamla spelet måste börja på 0 när man trycker på "kör igen"
+	}
+	}
+);
 
 
 buttonBack.addEventListener('click', () => {
@@ -100,10 +102,10 @@ startMeny.classList.add('visible')
 
 
 tryagainButton.addEventListener('click', () =>{
-startForButtonDiv.classList.remove('hidden');
-svårighetsgradDiv.classList.add('hidden');
-gameover.classList.remove('visible');
-gameoverButton.classList.remove('visible');
+	startForButtonDiv.classList.remove('hidden');
+	svårighetsgradDiv.classList.add('hidden');
+	gameover.classList.remove('visible');
+	gameoverButton.classList.remove('visible');
 })
 
 buttonBack.addEventListener('click', () =>{
@@ -132,6 +134,10 @@ function sparaNamn(){
 	userNameField.appendChild(newName)
 }
 
+// const gameState = {
+// 	playerName: 'David',
+// 	scores: []  // { name, correctGuesses, incorrectGuesses, ... }
+// }
 
 // nameInput.addEventListener("change", (event) => {
 // 	const namn = event.target.value; //hämta värdet från input
@@ -205,8 +211,12 @@ const legsGubbe = document.querySelector('#legs')
 const armsGubbe = document.querySelector('#arms')
 const bodyGubbe = document.querySelector('#body')
 const headGubbe = document.querySelector('#head')
+// const gubbe = { scaffold, legs, arms, body, head }
+// gubbe.legs
+// let gubbe = [scaffold, legs, arms, .. ]
 
 function startGame(){
+	gameState = 'game-view'
 	scaffoldGubbe.classList.add('invisible')
 	legsGubbe.classList.add('invisible')
 	armsGubbe.classList.add('invisible')
@@ -218,22 +228,18 @@ function startGame(){
 	correctGuess = 0
 	incorrectGuess = 0
 	totalGuess = 0
+	guessArray = []
 }
 
 lättButton.addEventListener('click', () => {
 	startGame()
 	chosenWord = easywords[randomInt(easywords.length)]
-	svårighetsgradDiv.classList.remove('visible');
-	svårighetsgradText.classList.add('hidden');
-	buttonBack.classList.add('visible'); 
 	gameplay()
 })
 
 mediumButton.addEventListener('click', () => {
 	startGame()
 	chosenWord = mediumwords[randomInt(mediumwords.length)]
-	svårighetsgradDiv.classList.remove('visible');
-	svårighetsgradText.classList.add('hidden');
 	gameplay()
 	
 })
@@ -241,32 +247,70 @@ mediumButton.addEventListener('click', () => {
 svårButton.addEventListener('click', () => {
 	startGame()
 	chosenWord = hardwords[randomInt(hardwords.length)]
-	svårighetsgradDiv.classList.remove('visible');
-	svårighetsgradText.classList.add('hidden');
 	gameplay()
 })
 
 //Skapar dashes ('_ _ _'), och ersätter dashes med bokstäver
 //när man trycker på rätt tangent:
 
+let guessArray = []
+
+// Helig David-text; vårda med omsorg
+
+function checkIfVictory(guesses, secretLetters) {
+	// guesses: lista med strängar, varje sträng är en bokstav
+	// secretLetters: lista med strängar, varje är en bokstav
+	for( let i=0; i<secretLetters.length; i++ ) {
+		let letter = secretLetters[i]
+		// Har vi gissat på den här bokstaven?
+		if( guessArray.includes(letter) ) {
+			// Bra, letter fanns. Kolla nästa
+		}
+		else {
+			// Aj då! Bokstaven har inte blivit gissat på!
+			return false
+		}
+	}
+	return true
+}
+
 function gameplay(){
 let displayedWord = chosenWord.replace(/./g, '<span class="dash">_</span>')
 const submissionField = document.getElementById('line-form')
 submissionField.innerHTML = displayedWord
 window.addEventListener('keyup', e => {
+	console.log('keyup', e.key, gameState);
+	if (gameState != 'game-view'){
+		return
+	}
 	let wordArray = chosenWord.split("")
 	let dashes = document.getElementsByClassName('dash')
 	if (wordArray.includes(e.key)){
-		wordArray.forEach((letter, index) => {
-			if (letter === e.key){
-				dashes[index].innerText = letter
+		tangent.forEach(t => {
+			if (t.innerText === (e.key).toUpperCase()) {
+					t.classList.add('correct')
+					}
+				})
+				wordArray.forEach((letter, index) => {
+					if (letter.toUpperCase() === e.key.toUpperCase()) {
+						dashes[index].innerText = letter.toUpperCase()
+					}
+				})
+				correctGuess++
+				guessArray.push(e.key)
 			}
-			})
-			correctGuess++
-		}
-		else{
+			else if (guessArray.includes(e.key)){
+				e.preventDefault()
+			}
+			else{
+			//const felTangent = ?
+			tangent.forEach(t => {
+				if (t.innerText === (e.key).toUpperCase()) {
+						t.classList.add('incorrect')
+						}
+					})
 			incorrectGuess++
-			console.log(incorrectGuess)
+			guessArray.push(e.key)
 			if (incorrectGuess === 1){
 				scaffoldGubbe.classList.remove('invisible')
 			}
@@ -282,13 +326,18 @@ window.addEventListener('keyup', e => {
 				legsGubbe.classList.remove('invisible')
 				gameover.classList.add('visible'); //Gameover popup!!!
 				gameoverButton.classList.add('visible')
-				totalGuess = incorrectGuess + correctGuess
+				tangent.forEach(t => {
+						t.classList.remove('correct')
+						t.classList.remove('incorrect')
+					})
+					gameState = 'game-over-view'
 			}
 	}
 })
 }
 
-
+// function updateGubbeIncorrectGuess() {}
+// function updateGubbeCorrectGuess() {}
 
 
 
