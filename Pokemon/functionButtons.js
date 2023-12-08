812cdaf6f1f9589b3c75cae8261ce251c50e4481
HannisGo1 <Hannakarlsson69@gmail.com>
2023-12-08 19:23:16 +0100

    css html js

---
+++ Pokemon/functionButtons.js
@@ -0,0 +1,8 @@
+export { FindChampionButton, MyTeamButton }
+FindChampionButton.addEventListener('click', async () => {
+	Event.stopPropagation
+});
+
+MyTeamButton.addEventListener('click', async () => {
+	Event.stopPropagation
+});
