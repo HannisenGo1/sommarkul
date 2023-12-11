197ee7d0f6b2d666d6f4bbbe10a35ee611747cb4
HannisGo1 <Hannakarlsson69@gmail.com>
2023-12-11 21:02:36 +0100

    tillagda rader

--- Pokemon/variabler.js
+++ Pokemon/variabler.js
@@ -2,20 +2,26 @@
 //import { getAdvice } from './ajax.js'
 
 const serchMeny = document.querySelector('.sök-container');
-const findChampionButton = document.getElementById('championbutton'); //knappen för find champion
+const findChampionButton = document.querySelector('.championbutton'); //knappen för find champion
 const divFindChampionButton= document.querySelector('.findChampionstart');
-const myTeamButton = document.getElementById('team-button')
+const myTeamButton = document.querySelector('.team-button')
+//knappen för my Team
 const divMyTeamButton = document.querySelector('.MyTeamstart');
-const menyChampion =document.getElementById('championMenyn')// menyn för champions
+const menyChampion =document.querySelector('.championMenyn')// menyn för champions
 const images = document.querySelector('.image') //bilder på pokemon
-const addPokemonsButton = document.getElementById('addPokemonsbutton') //lägg till pokemon med knapp
-const myTeam = document.getElementById('my-team');// meny för ens team 
+const addPokemonsButton = document.querySelector('.addPokemonsbutton') //lägg till pokemon med knapp
+const buttonsearch = document.getElementById('searchbutton'); //sök knapp för pokemonen
+const myTeam = document.querySelector('.my-team'); // meny för mitt team 
 const backmeny = document.querySelector('.backMenyn');
-const backButton = document.getElementById('back-button');
+const backButton = document.querySelector('.back-button');
+const divSearchForPokemon = document.querySelector('.searchForPokemonDiv')
 
-menyChampion.classList.add('hidden') //menyn champion hidden
-myTeam.classList.add('hidden'); //menyn my team hidden
+myTeam.classList.add('hidden');
+menyChampion.classList.add('hidden'); //menyn champion hidden
 backmeny.classList.add('hidden');
+serchMeny.classList.add('hidden');
+//divSearchForPokemon.classList.add('hidden')
+
 
 
 findChampionButton.addEventListener('click', async () => {
@@ -35,13 +41,20 @@ myTeamButton.addEventListener('click', async () => {
 	backButton.classList.remove('hidden');
 	divFindChampionButton.classList.add('hidden');
 	backmeny.classList.remove('hidden');
+	findChampionButton.classList.add('hidden')
 });
+
 backButton.addEventListener('click', async () => { 
 	divFindChampionButton.classList.remove('hidden');
 	divMyTeamButton.classList.remove('hidden');
 	backmeny.classList.add('hidden');
-	menyChampion.classList.add('hidden');
+	findChampionButton.classList.remove('hidden');
+	menyChampion.classList.remove('hidden');
 	myTeam.classList.add('hidden');
+	divFindChampionButton.classList.add('visible');
+	serchMeny.classList.add('hidden');
+	divSearchForPokemon.classList.add('hidden');
+
  });
 
 
