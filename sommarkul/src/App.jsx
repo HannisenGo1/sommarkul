e1c0d3c3b5044a33b0724154966437e04d78f658
HannisGo1 <Hannakarlsson69@gmail.com>
2024-04-23 20:05:04 +0200

    tillagda filer

---
+++ sommarkul/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import './mediaQ.css'
+import Frontpage from "../src/components/Frontpage"
+
+
+function App() {
+  
+
+  return (
+    <>
+	<Frontpage />
+	
+	
+   
+    </>
+  )
+}
+
+export default App
---
+++ sommarkul/src/components/Frontpage.jsx
@@ -0,0 +1,17 @@
+import sol from "../data/img/sol.png"
+
+
+const Frontpage = () => {
+
+
+
+return ( 
+<div className='theHeader'> 
+<img className="icon" src={sol} alt="Icon" />
+		<h1> Sommarkul </h1> 
+	</div>
+	
+	)
+}
+
+export default Frontpage
